5a4ce623d582506bf5714929f0ecdbe6aabc6732
Alexander Stark <131863712+lallaxstudies@users.noreply.github.com>
2024-04-30 12:39:16 +0200

    Initialisierungsdateien für die DB
    initDB.sql erstellt die benötigten Tabellen
    testData.sql enthält jeweils 3 Datensätze pro Tabelle. !Hinweis! Passwörter sind noch in Klarschrift und nicht gehasht

---
+++ DB_init.sql
@@ -0,0 +1,28 @@
+-- Erstelle die Projekte-Tabelle
+CREATE TABLE Projects (
+    project_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    project_name TEXT NOT NULL,
+    description TEXT,
+    board_name TEXT NOT NULL
+);
+
+-- Erstelle die Benutzer-Tabelle
+CREATE TABLE Users (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+);
+
+-- Erstelle die Aufgaben-Tabelle
+CREATE TABLE Tasks (
+    task_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    task_title TEXT NOT NULL,
+    description TEXT,
+    due_date TEXT,
+    status TEXT,
+    assignee_id INTEGER,
+    project_id INTEGER,
+    FOREIGN KEY(assignee_id) REFERENCES Users(user_id),
+    FOREIGN KEY(project_id) REFERENCES Projects(project_id)
+);
---
+++ testData.sql
@@ -0,0 +1,23 @@
+-- Beispieldatensätze für die Benutzer-Tabelle
+
+INSERT INTO Users(username, email, pass) VALUES ('Alex','test@gmail.com','admin1234!'); --Password is 'admin1234!'
+INSERT INTO Users(username, email, pass) VALUES ('John','john@gmail.com','ICHBINJOHN'); --Password is 'ICHBINJOHN'
+INSERT INTO Users(username, email, pass) VALUES ('Claire','claire@fromthewoods.com','IlikeTrees'); --Password is 'IlikeTrees'
+
+-- Beispieldatensätze für die Projekte-Tabelle
+
+INSERT INTO Projects(project_name, description, board_name) VALUES ('TestProjekt','Projekt zum Testen','Das erste Board');
+INSERT INTO Projects(project_name, description, board_name) VALUES ('bestprojectever','ich bin so cool','cooles Board');
+INSERT INTO Projects(project_name, description, board_name) VALUES ('Projekt3','','Board ohne Beschreibung');
+
+-- Beispieldatensätze für die Aufgaben-Tabelle
+
+INSERT INTO Tasks (task_title, description, due_date, status, assignee_id, project_id) VALUES ('Aufgabe 1', 'Beschreibung für Aufgabe 1', '2024-05-01 12:00:00', 'ToDo', 1, 1);
+INSERT INTO Tasks (task_title, description, due_date, status, assignee_id, project_id) VALUES ('Aufgabe 2', 'Beschreibung für Aufgabe 2', '2024-05-02 12:00:00', 'In Progress', 2, 1);
+INSERT INTO Tasks (task_title, description, due_date, status, assignee_id, project_id) VALUES ('Aufgabe 3', 'Beschreibung für Aufgabe 3', '2024-05-03 12:00:00', 'Done', 3, 2);
+
+-- Aktualisiere die Tasks-Tabelle mit Fremdschlüsseln
+
+UPDATE Tasks SET assignee_id = 1 WHERE task_id = 1; -- Bezieht sich auf Alex
+UPDATE Tasks SET assignee_id = 2 WHERE task_id = 2; -- Bezieht sich auf John
+UPDATE Tasks SET assignee_id = 3 WHERE task_id = 3; -- Bezieht sich auf Claire
