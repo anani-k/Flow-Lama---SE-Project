a6228ca0efe2889c08f408c5c1e8efa801aca492
lallaxstudies <alexander.stark@haw-hamburg.de>
2024-06-04 10:04:09 +0200

    Commiting config changes

--- app.js
+++ app.js
@@ -34,4 +34,4 @@ app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
 
-module.exports = { app, myEmitter };
+module.export = { app, myEmitter };
--- db.js
+++ db.js
@@ -2,5 +2,9 @@
 
 const DATABASE = "database.db";
 const db = require("better-sqlite3")(DATABASE);
+/*const initialization = fs.readFileSync('DB_init.sql', 'utf8');
+const testData = fs.readFileSync('testData.sql', 'utf8');
+db.exec(initialization);
+db.exec(testData);*/
 
 module.exports = db;
